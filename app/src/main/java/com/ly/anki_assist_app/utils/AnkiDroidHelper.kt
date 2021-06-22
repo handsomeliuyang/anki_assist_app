f5363a30d41c8cb3e3af0f3bf4f483b809534160
liuyang <liuyang@58.com>
2021-06-22 23:00:41 +0800

    添加两次准备方法

--- app/src/main/java/com/ly/anki_assist_app/utils/AnkiDroidHelper.kt
+++ app/src/main/java/com/ly/anki_assist_app/utils/AnkiDroidHelper.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import com.ichi2.anki.api.AddContentApi
 import com.ly.anki_assist_app.App
 
+const val ANKI_PACKAGE_NAME = "com.ichi2.anki"
+
 class AnkiDroidHelper private constructor() {
 
     companion object {
@@ -15,6 +17,22 @@ class AnkiDroidHelper private constructor() {
 
     fun init(context: Context) {
         mAnkiApi = AddContentApi(context)
+    }
+
+    fun isApiAvailable(): Boolean{
+        return AddContentApi.getAnkiDroidPackageName(App.context) != null
+    }
+
+    fun startAnkiDroid(): Boolean{
+        val manager = App.context.getPackageManager()
+        val intent = manager.getLaunchIntentForPackage(ANKI_PACKAGE_NAME)
+        if(intent == null){
+            return false
+        }
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        App.context.startActivity(intent)
+        return true
     }
 
     fun getDeckList(){
